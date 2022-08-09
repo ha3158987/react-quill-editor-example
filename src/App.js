8038013a0892e40498aa15dd2d621c35d1c47c4f
Hannah Park <ha3158987@gmail.com>
2022-08-10 00:39:23 +0900

    feat: create react-quill library test example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Editor from "./Editor";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <body>
+        <Editor />
+      </body>
     </div>
   );
 }
---
+++ src/Editor.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import styled from "styled-components";
+
+const modules = {
+  toolbar: [
+    //[{ 'font': [] }],
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+    ["clean"],
+  ],
+};
+
+const formats = [
+  //'font',
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "align",
+  "color",
+  "background",
+];
+
+function Editor() {
+  const [value, setValue] = useState("");
+
+  const handleChange = (content, delta, source, editor) => {
+    // console.log(editor.getHTML()); // html 사용시
+    console.log(JSON.stringify(editor.getContents())); // delta 사용시
+    setValue(editor.getHTML());
+  };
+
+  return (
+    <Cotainer>
+      <ReactQuill
+        style={{ height: "600px" }}
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={value}
+        onChange={handleChange}
+      />
+    </Cotainer>
+  );
+}
+
+const Cotainer = styled.div`
+  height: 650px;
+`;
+
+export default Editor;
